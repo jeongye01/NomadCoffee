c95c626aaf156a328f96c02ff2d1c0305faceaed
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-10-01 22:22:17 +0900

    assignment2

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 import {ApolloServer} from "apollo-server";
 import schema from "./schema";
+
 const server=new ApolloServer({schema});
 
 const PORT=process.env.PORT;
+console.log(PORT);
 server.listen(PORT).
 then(()=>console.log(`Server is running on http://localhost:${PORT}/`));
